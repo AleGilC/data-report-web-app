8be52eb041a13c75189e4913c8fd4260678241ad
alegilc <gilc.alejandro@gmail.com>
2021-04-01 20:23:54 -0400

    Settings

--- client/web.client/nuxt.config.js
+++ client/web.client/nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
 import lang from './lang';
 
 export default {
@@ -7,18 +6,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - web.client',
-    title: 'web.client',
-    htmlAttrs: {
-      lang: 'en',
-    },
+    titleTemplate: '%s - ' + 'Master Report Data',
+    title: 'Master Report Data',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'Master Report Data',
+        content: 'Sistema centralizado para manejar data',
       },
     ],
     link: [
@@ -40,36 +36,55 @@ export default {
           'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@latest/css/all.min.css',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/toast'],
+  plugins: [
+    '@/plugins/toast',
+    '@/plugins/alert',
+    // '@/plugins/pdfmake',
+    // '@/plugins/axios',
+    // '@/plugins/signalrHub',
+    // '@/plugins/components',
+    // '@/services/api',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: false,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/vuetify
     '@nuxtjs/vuetify',
+    ['@nuxtjs/dotenv', { filename: `.env.${process.env.APP_ENV}` }],
+    '@nuxtjs/moment',
   ],
+
+  loading: {
+    color: '#29a19a',
+    height: '4px',
+  },
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
+    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-
-    'nuxt-sweetalert2',
-
     'nuxt-i18n',
+    [
+      'nuxt-sweetalert2',
+      {
+        confirmButtonColor: '#29a19a',
+        cancelButtonColor: '#ff4848',
+      },
+    ],
   ],
-
+  moment: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
   i18n: {
     locales: Object.keys(lang),
     defaultLocale: 'es',
@@ -80,27 +95,19 @@ export default {
   },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    credentials: true,
+    debug: false,
+  },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['@/assets/vuetify-conf.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
+    optionsPath: '@/plugins/vuetify.options.js',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    analyze: true,
+  },
 };
